ac97f96679e341d833a4952c21bbf5161fc771c9
Antoine <antoine.lucas@nickel.eu>
2021-06-02 22:02:18 +0200

    Add remaining components in example

--- src/main/webapp/features/studio/fixture.ts
+++ src/main/webapp/features/studio/fixture.ts
@@ -19,18 +19,9 @@
 
 export const Basic = `name: Basic
 
+name: Basic
+
 components:
-  - name: Frontend
-    label: Customer Portal
-    description: The customer portal frontend
-    type: frontend
-    dependencies:
-      - component: API
-      - component: Library
-
-  - name: Library
-    type: library
-
   - name: API
     type: service
     context: context1
@@ -38,14 +29,50 @@ components:
       - component: Database
       - component: Queue
         type: queue.publish
+      - component: Stored-procedure
+
+  - name: Cache
+    type: cache
+    dependencies:
+      - component: API
+
+  - name: Config
+    type: config
 
   - name: Database
     type: database
     context: context1
 
+  - name: Frontend
+    type: frontend
+    dependencies:
+      - component: Gateway
+      - component: Config
+      - component: Library
+
+  - name: Function
+    type: function
+    context: context2
+    dependencies:
+      - component: S3
+
+  - name: Gateway
+    type: gateway
+    dependencies:
+      - component: Cache
+      - component: Secret
+
+  - name: Library
+    type: library
+
   - name: Queue
     type: queue
     context: context1
+
+  - name: S3
+    label: Amazon S3
+    type: object-storage
+    context: context2
 
   - name: Script
     type: script
@@ -53,10 +80,15 @@ components:
     dependencies:
       - component: Queue
         type: queue.subscribe
-      - component: s3
+      - component: S3
+      - component: Function
 
-  - name: s3
-    label: Amazon S3
-    type: object-storage
-    context: context2
+  - name: Secret
+    type: secret
+
+  - name: Stored-procedure
+    type: stored-procedure
+    context: context1
+    dependencies:
+      - component: Database
 `;
